539aaadb5683848bf613517e3c6c0b0c3f89491c
= <qais.gh11@gmail.com>
2017-09-05 23:24:51 -0500

    test functions

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,5 +1,17 @@
 angular.module('myGame')
 .controller('main-controller',function($scope, mainService,$state ){
 $scope.goToPlay = mainService.goToPlay
-$scope.testFunc = mainService.testFunc
+// $scope.testFunc = mainService.testFunc
+$scope.rock = 'hi'
+var gameData = {
+    rock: 'https://cdn.pixabay.com/photo/2013/07/13/09/51/rock-paper-scissors-156171_1280.png'
+} 
+$scope.imgSrc = 'https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296854_1280.png'
+
+$scope.getVal = function(str) {
+    if(!str) {
+        return
+    }
+    $scope.imgSrc = gameData[str]
+}
 });
--- js/main-service.js
+++ js/main-service.js
@@ -3,13 +3,14 @@ angular.module('myGame')
     this.goToPlay = function(){
         $state.go('game')
     }
-    this.testFunc = function(){
+    this.testFunc = function(){ 
         var imgRock = document.createElement('img')
         var imgContainer = document.getElementsByClassName('img-container')
         imgRock.setAttribute('src', 'https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296854_640.png')
         
-        console.log(imgRock)
-        imgContainer.appendChild(imgRock);
+        console.log(imgContainer)
+        imgContainer[0].appendChild(imgRock);
     }
+
 });
     
