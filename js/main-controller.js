e7f8d3b898c17dbdea3b99cf3a2ee3508f3d5222
= <qais.gh11@gmail.com>
2017-09-08 15:01:03 -0500

    seeting up the counter

--- js/main-controller.js
+++ js/main-controller.js
@@ -7,5 +7,7 @@ $scope.randomVal = '../assets/all.jpg';
 $scope.gameData = mainService.gameData;
 $scope.getVal = mainService.getVal;
 $scope.pickRandomVal = mainService.pickRandomVal;
+$scope.count = mainService.count;
 $scope.score = mainService.score;
+$scope.timer = mainService.timer;
 });
--- js/main-service.js
+++ js/main-service.js
@@ -6,7 +6,7 @@ angular.module('myGame')
         scissors:'../assets/s.jpg'
     };
     this.score = 0;
-
+    this.timer = 50;
     this.goToPlay = function(){
         $state.go('game');
     };
@@ -14,13 +14,21 @@ angular.module('myGame')
         if(!key) {
             return
         }
-        this.imgSrc = this.gameData[key]
+        this.imgSrc = this.gameData[key]        
     };
     
     this.pickRandomVal = function(obj) {
         var keys = Object.keys(obj)
-        var key = obj[keys[ keys.length * Math.random() << 0]]
+        var key = obj[keys[ keys.length * Math.random() << 0]]        
         this.randomVal = key
     };
+    this.count = function(num){
+        for(var i = 0 ; i <= num; i++){ 
+            if(num!== 0){
+                num --
+            }
+        }
+        return this.count(num -1)
+    }
     
 });
